33130157620bcde3d72e82a6df2faf75a55c80ec
Ana Garces <anagarcestech@gmail.com>
2025-02-10 13:24:54 +0100

    Solucion ejercicio 32

--- src/main/java/com/campusdual/classroom/Exercise32.java
+++ src/main/java/com/campusdual/classroom/Exercise32.java
@@ -2,31 +2,44 @@ package com.campusdual.classroom;
 
 import com.campusdual.util.Utils;
 
-import java.io.BufferedWriter;
-import java.io.IOException;
-import java.io.PrintWriter;
+import java.io.*;
 
 public class Exercise32 {
 
     public static void main(String[] args) {
 
+        printToFile(generateStringToSave("This is a\nmulti-line string."));
     }
 
     public static String generateStringToSave(String string) {
-
+        if (string == null) {
+            return generateUserInputToSave();
+        }
+        return string;
     }
 
-    private static String generateUserInputToSave(){
+    private static String generateUserInputToSave() {
         StringBuilder sb = new StringBuilder();
         System.out.println("Escribe debajo el texto que quieras. Pulsa \"ENTER\" 2 veces seguidas para finalizar:");
         String string;
-        while(!(string = Utils.string()).isEmpty()){
+        while (!(string = Utils.string()).isEmpty()) {
             sb.append(string).append(System.lineSeparator());
         }
         return sb.toString();
     }
 
     public static void printToFile(String string) {
+
+        File file = new File("src/main/resources/data.txt");
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter(new FileWriter(file));
+            pw.println(string);
+            pw.close();
+
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
 
     }
 
